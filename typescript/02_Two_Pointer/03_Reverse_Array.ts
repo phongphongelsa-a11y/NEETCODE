63a76685d32eed0c0a3ea639d7693ca815e7e187
Phong <phongphong.elsa@gmail.com>
2026-07-22 07:18:45 +0700

    Update reverse Array

---
+++ typescript/02_Two_Pointer/03_Reverse_Array.ts
@@ -0,0 +1,15 @@
+function Reverse_Array(nums:number[]) : number[] {
+    let left = 0;
+    let right = nums.length -1;
+    while (left<right) {
+        let temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
+        left++;
+        right--; 
+    }
+    return nums;
+}
+
+const array:number[] = [1,2,3,4,5];
+console.log(Reverse_Array(array));
