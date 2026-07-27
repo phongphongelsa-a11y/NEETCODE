8de68d31d301775e47e00e23f3bae76cb59e38c0
Phong <phongphong.elsa@gmail.com>
2026-07-27 17:26:20 +0700

    Update sliding window

--- typescript/02_Two_Pointer/10_Maximum_SubArray_Size_of_K.ts
+++ typescript/02_Two_Pointer/10_Maximum_SubArray_Size_of_K.ts
@@ -10,6 +10,21 @@ function Maximum_SubArray_Size_of_K_Brute_Force(nums:number[], k:number) : numbe
     return sumMax;
 }
 
+function Maximum_SubArray_Size_of_K_Sliding_Window(nums:number[], k:number): number {
+    let window_Sum = 0;
+    let max_Sum = 0;
+    for (let i =0;i<nums.length;i++) {
+        window_Sum+=nums[i];
+    }
+
+    for (let i = k;i<nums.length;i++) {
+        window_Sum+=window_Sum+ nums[i] - nums[i-k];
+        max_Sum = Math.max(window_Sum, max_Sum);
+    }
+
+    return max_Sum;
+    
+}
 let array = [2,1,5,1,3,2];
 let k = 3;
 console.log(Maximum_SubArray_Size_of_K_Brute_Force(array,k));
