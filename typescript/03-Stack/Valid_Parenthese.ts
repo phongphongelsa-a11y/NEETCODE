9c71b8d6cbcbc9d1ab40580d0d65b62e927989d3
Phong <phongphong.elsa@gmail.com>
2026-08-18 13:56:05 +0700

    Update comment

--- typescript/03-Stack/Valid_Parenthese.ts
+++ typescript/03-Stack/Valid_Parenthese.ts
@@ -1,4 +1,9 @@
 function brute_force(s: string) : boolean {
+    // Time: o(n^2)
+    // Space: o(n);
+
+    // Lặp cho đến khi các cặp ngoặc được thay thế bằng '' -> Nếu chuỗi rỗng thì true, nếu còn thì false
+
     while (s.includes('[]') || s.includes('{}') || s.includes('()')) {
         s = s.replace('[]','');
         s = s.replace('{}','');
@@ -8,7 +13,32 @@ function brute_force(s: string) : boolean {
 }
 
 function Valid_Parenthese(s:string) : boolean {
+    // Time: o(n);
+    // Space: o(n);
+    // Tạo map các cặp ngoặc mở và đóng -> duyệt s -> nếu có ngoặc mở thì push ngoặc đóng vào stack -> nếu ko phải ngoặc mở
+    // -> kiểm tra xem đỉnh stack có phải là ngoặc đóng ko, nếu ko phải thì return false -> return true nếu stack rỗng 
     let stack = [];
+    const map = new Map<string,string>([
+        ['[',']'],
+        ['{','}'],
+        ['(',')']
+    ]);
+
+
+    for (let char of s) {
+        if (map.has(char)) {
+            stack.push(map.get(char));
+        }
+        else {
+            if (char !== stack.pop()) {
+                return false;
+            }
+        }
+    }
+    return stack.length === 0;
+
+
+
 
     // for (let c of s ) {
     //     if (c == '[') {
@@ -28,25 +58,6 @@ function Valid_Parenthese(s:string) : boolean {
     // }
     // return stack.length === 0;
     
-    
-    const map = new Map<string,string>([
-        ['[',']'],
-        ['{','}'],
-        ['(',')']
-    ]);
-
-
-    for (let char of s) {
-        if (map.has(char)) {
-            stack.push(map.get(char));
-        }
-        else {
-            if (char !== stack.pop()) {
-                return false;
-            }
-        }
-    }
-    return stack.length === 0;
 }
 
 
